d6dbd2e58aa2265dd4b7dece1f0b0af88748d3de
schwarzwald0906 <93188453+schwarzwald0906@users.noreply.github.com>
2022-08-07 17:39:14 +0900

    微修正

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,11 @@ export default function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <Link to="./">Home</Link>
+        <Link to="/">Home</Link>
         <br />
-        <Link to="./Page1">Page1</Link>
+        <Link to="/Page1">Page1</Link>
         <br />
-        <Link to="./Page2">Page2</Link>
+        <Link to="/Page2">Page2</Link>
       </div>
       <Switch>
         <Route exact path="/">
