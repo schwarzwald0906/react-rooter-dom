6b0d7cf0367f8cd428bf2ed2e2dd07eefa6c4869
schwarzwald0906 <93188453+schwarzwald0906@users.noreply.github.com>
2022-08-08 14:36:33 +0900

    Linkを使わないページ遷移

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,8 +1,10 @@
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 export const Page1 = () => {
   const Arr = [...Array(100).keys()];
   console.log(Arr);
+  const history = useHistory();
+  const ButtonPage1detailA = () => history.push("/Page1/Page1detailA");
   return (
     <div>
       <h1>Page1ページです</h1>
@@ -11,6 +13,8 @@ export const Page1 = () => {
       </Link>
       <br />
       <Link to="/Page1/Page1detailB">Page1detailB</Link>
+      <br />
+      <button onClick={ButtonPage1detailA}>Page1detailA</button>
     </div>
   );
 };
